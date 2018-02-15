3c4942434215aaa0bfb26bd5b15f6025dd233f97
Jozef Dransfield <Jozef.Dransfield@me.com>
2018-02-15 22:15:12 +0000

    Device Tests

--- src/main/kotlin/dyson/Device.kt
+++ src/main/kotlin/dyson/Device.kt
@@ -7,11 +7,12 @@ import org.fusesource.mqtt.client.BlockingConnection
 import org.fusesource.mqtt.client.MQTT
 import org.fusesource.mqtt.client.QoS
 import org.fusesource.mqtt.client.Topic
+import java.io.Closeable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.function.Consumer
 
-class Device(hostAddress: String, port: Int, password: String, private val metaData: DeviceMetaData) {
+class Device(hostAddress: String, port: Int, password: String, metaData: DeviceMetaData, mqtt: MQTT) : Closeable {
 
     private val connection: BlockingConnection
     private val executor: ExecutorService = Executors.newFixedThreadPool(1)
@@ -20,8 +21,9 @@ class Device(hostAddress: String, port: Int, password: String, private val metaD
 
     private val baseTopic = "${metaData.productType}/${metaData.serial}"
 
+    constructor(hostAddress: String, port: Int, password: String, metaData: DeviceMetaData) : this(hostAddress, port, password, metaData, MQTT())
+
     init {
-        val mqtt = MQTT()
         mqtt.setHost(hostAddress, port)
         mqtt.setUserName(metaData.serial)
         mqtt.setPassword(password)
@@ -54,7 +56,7 @@ class Device(hostAddress: String, port: Int, password: String, private val metaD
         receivers.add(consumer)
     }
 
-    fun close() {
+    override fun close() {
         executor.shutdownNow()
         connection.disconnect()
     }
---
+++ src/test/kotlin/dyson/DeviceSpek.kt
@@ -0,0 +1,61 @@
+package dyson
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.nhaarman.mockito_kotlin.*
+import org.awaitility.Awaitility.await
+import org.fusesource.mqtt.client.BlockingConnection
+import org.fusesource.mqtt.client.MQTT
+import org.fusesource.mqtt.client.Message
+import org.fusesource.mqtt.client.QoS
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import java.util.*
+import java.util.concurrent.Callable
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+import java.util.function.Supplier
+import kotlin.concurrent.timerTask
+
+class DeviceSpek : Spek({
+    describe("a device") {
+        val message = mock<Message>{
+            on {payload} doReturn """{}""".toByteArray()
+        }
+        val connection = mock<BlockingConnection>{
+            on{receive()} doAnswer {
+                Thread.sleep(100)
+                message
+            }
+        }
+        val mqtt = mock<MQTT> {
+            on {blockingConnection()} doReturn connection
+        }
+        describe("the constructor") {
+            it("connects to mqtt") {
+                Device("host", 1, "password", DeviceMetaData(), mqtt).use {}
+            }
+        }
+        describe("an instance") {
+            it("should send a command") {
+                Device("host", 1, "password", DeviceMetaData(productType = "productType", serial = "serial"), mqtt).use {
+                    it.sendCommand("payload")
+                }
+                verify(connection).publish("productType/serial/command", "payload".toByteArray(), QoS.AT_LEAST_ONCE, false)
+            }
+
+            it("should receive a message") {
+                Device("host", 1, "password", DeviceMetaData(productType = "productType", serial = "serial"), mqtt).use {
+                    var message : JsonNode? = null
+                    it.receive(Consumer {
+                        message = it
+                    })
+                    await().atMost(1, TimeUnit.SECONDS).until({message}, not(nullValue()))
+                }
+            }
+        }
+
+    }
+})
